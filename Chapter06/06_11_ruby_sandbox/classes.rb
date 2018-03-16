6ac70f47ff140f03184376e3a048d29c3ec8cdfe
Kenneth Barrett <kenneth.barrett.jr@gmail.com>
2018-03-15 21:01:59 -0400

    Chapter 7 exercises

--- Chapter06/06_11_ruby_sandbox/classes.rb
+++ Chapter06/06_11_ruby_sandbox/classes.rb
@@ -46,6 +46,9 @@ class Animal
 end
 
 class Cow < Animal
+  def color
+    "The cow's color is #{@color}."
+  end
 end
 
 Animal.species = ['frog', 'fish']
@@ -65,7 +68,8 @@ puts animal2.color
 
 puts Animal.current_animals.inspect
 
-maisie = Cow.new("Moo!")
+maisie = Cow.create_with_attributes("Moo!","yellow")
 puts maisie.noise
 puts animal1.class
 puts maisie.class
+puts maisie.color
